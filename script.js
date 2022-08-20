810eef8ce6fd6b20bebc4bdffa21b837439cf618
Zaratosh13 <105578171+Zaratosh13@users.noreply.github.com>
2022-08-20 16:23:27 +0530

    Memebot

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿const kt = () => {
+    fetch("https://meme-api.herokuapp.com/gimme")
+        .then((data) => data.json())
+        .then((data_val) => {
+            const u_data = data_val.url;
+            document.getElementById("myImg").src = u_data;
+            console.log(u_data);
+        });
+};
+kt();
+
+
+const myfunc = () => {
+    kt();
+}
